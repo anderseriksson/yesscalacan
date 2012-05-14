986b001ff6ba85146443b2b9e69662d8eeda9c53
Anders Eriksson <anders.eriksson@gmail.com>
2012-05-14 07:56:07 +0200

    base line for rock paper scissors example

--- src/main/scala/RockScissorsPaper.scala
+++ src/main/scala/RockScissorsPaper.scala
@@ -12,13 +12,18 @@ class RockScissorsPaper {
 
     (a, b) match {
       case ("Rock", "Scissors") => "Rock wins"
+      case ("Rock", "Paper") => "Paper wins"
       case ("Scissors", "Paper") => "Scissors wins"
+      case ("Scissors", "Rock") => "Rock wins"
       case ("Paper", "Rock") => "Paper wins"
+      case ("Paper", "Scissors") => "Scissors wins"
       case (x, y) if x == y => "Draw!"
       case _ => "Invalid input!"
     }
 
   }
+
+  
 
 
 }
