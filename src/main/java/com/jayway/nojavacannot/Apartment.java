6ae6276000e94d5aa7b6b18e18b70c166ff19196
Anders Eriksson <anders.eriksson@gmail.com>
2012-05-21 22:36:50 +0200

    made java version more verbose

--- src/main/java/com/jayway/nojavacannot/Apartment.java
+++ src/main/java/com/jayway/nojavacannot/Apartment.java
@@ -8,9 +8,9 @@ package com.jayway.nojavacannot;
  */
 public class Apartment {
 
-    private int rooms;
-    private boolean hasBalcony;
-    private boolean hasWasher;
+    final private int rooms;
+    final private boolean hasBalcony;
+    final private boolean hasWasher;
 
     public Apartment(int rooms, boolean hasBalcony, boolean hasWasher) {
         this.rooms = rooms;
@@ -25,5 +25,61 @@ public class Apartment {
 //    public Apartment(int rooms, boolean hasWasher) {
 //        this(rooms, false, hasWasher);
 //    }
+
+    /**
+     * More factories are created in Scala.
+     * @param rooms
+     * @param hasBalcony
+     * @param hasWasher
+     * @return
+     */
+    public static Apartment factory(int rooms, boolean hasBalcony, boolean hasWasher) {
+        return new Apartment(rooms, hasBalcony, hasWasher);
+    }
+
+    
+    public int getRooms() {
+        return rooms;
+    }
+
+    public boolean isHasBalcony() {
+        return hasBalcony;
+    }
+
+    public boolean isHasWasher() {
+        return hasWasher;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Apartment)) return false;
+
+        Apartment apartment = (Apartment) o;
+
+        if (hasBalcony != apartment.hasBalcony) return false;
+        if (hasWasher != apartment.hasWasher) return false;
+        if (rooms != apartment.rooms) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = rooms;
+        result = 31 * result + (hasBalcony ? 1 : 0);
+        result = 31 * result + (hasWasher ? 1 : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Apartment[" +
+                "rooms=" + rooms +
+                ", hasBalcony=" + hasBalcony +
+                ", hasWasher=" + hasWasher +
+                ']';
+    }
+    
 
 }
